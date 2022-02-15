844f0af95dfa3092a34efd1430f8ae9f4f60d71c
Gavin <Gavinszczesniak@gmail.com>
2022-02-15 08:12:50 -0500

    Finished challenge 2 last night, forgot to push it.

--- script.js
+++ script.js
@@ -11,3 +11,29 @@ buttons.forEach((btn) => {
     total.textContent = `Total: $${(totalCost += getCost)}.00`;
   });
 });
+
+const buttonMoney = document.querySelector(".two button");
+const divCoinToAttach = document.querySelector(".coin-container");
+const removeCoins = [];
+
+buttonMoney.addEventListener("click", (e) => {
+  e.preventDefault();
+  const amount = document.querySelector("#amount").value;
+  const coinType = document.querySelector("#coin").value;
+  for (let i = 0; i < amount; i++) {
+    let coin = document.createElement("div");
+    coin.classList.add(coinType, `coin`);
+    let coinPara = document.createElement("p");
+    coinPara.textContent = coinType;
+    divCoinToAttach.append(coin);
+    coin.append(coinPara);
+    removeCoins.push(document.querySelector(".coin"));
+  }
+});
+
+removeCoins.forEach((coin) => {
+  coin.addEventListener("click", () => {
+    console.log("delete");
+    coin.remove();
+  });
+});
